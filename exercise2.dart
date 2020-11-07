2320d54406919b1787a23e829bde5626837e0f25
LeNinjaHD <58134535+LeNinjaHD@users.noreply.github.com>
2020-11-07 14:45:21 +0100

    Create exercise2.dart

---
+++ exercise2.dart
@@ -0,0 +1,10 @@
+void main() {
+  String meinName = "Cedric";
+  print("Mein Name ist $meinName");
+  
+  double d = 1.65464567576455;
+  String doubleString = d.toString();
+  print(doubleString);
+  int meinNamelength = meinName.length;
+  print("Die Länge meines Name ist $meinNamelength");
+}
